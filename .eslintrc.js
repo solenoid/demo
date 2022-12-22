896fed49154e41baa1b3d3e7c7fcad3dfbf3bb32
Erik Solen <erik@solen.dev>
2022-12-22 14:36:01 -0500

    fix lint and stricter typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   // See https://eslint.org/docs/latest/developer-guide/shareable-configs
   // for npm scoped and prefix naming convention rules
-  extends: ['@solenoid'],
+  extends: ['@config'],
   settings: {
     next: {
       rootDir: ['apps/*/'],
--- libs/ui/src/Button.tsx
+++ libs/ui/src/Button.tsx
@@ -1,8 +1,15 @@
-// import d3 from 'd3';
+// import d3 from 'd3'
+// const unused = true
 const buttonName = ['beep', 'bop', 'boop']
 export const Button = (props: any) => {
   // return <button>Boop</button>;
-  // return buttonName.map(name => <button>{name}</button>)
+  // return (
+  //   <>
+  //     {buttonName.map((name) => (
+  //       <button>{name}</button>
+  //     ))}
+  //   </>
+  // )
   return (
     <>
       {props.label}
