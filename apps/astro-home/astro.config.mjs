1d0209f21415c3ce9f6272383e12092fe63c7ef4
Erik Solen <erik@solen.dev>
2023-01-07 11:51:58 -0500

    small things

--- apps/astro-home/astro.config.mjs
+++ apps/astro-home/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  // server: { port: 3000 },
   integrations: [react()],
   output: 'server',
   adapter: node({
