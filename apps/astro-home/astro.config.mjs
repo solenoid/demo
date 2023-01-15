b0a71fbe6c3a5c346586873c616a9aecccb642a2
Erik Solen <erik@solen.dev>
2023-01-15 11:07:45 -0500

    move astro to port 9000

--- apps/astro-home/astro.config.mjs
+++ apps/astro-home/astro.config.mjs
@@ -2,9 +2,14 @@ import node from '@astrojs/node'
 import react from '@astrojs/react'
 import { defineConfig } from 'astro/config'
 
+// For local dev port
+const port = 9000
+
 // https://astro.build/config
 export default defineConfig({
-  // server: { port: 3000 },
+  // NOTE trailing slash is added if you don't include it
+  site: `http://localhost:${port}/`,
+  server: { port },
   integrations: [react()],
   output: 'server',
   adapter: node({
