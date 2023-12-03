1d227ab3d2ef0519dd731dd7ff9f98169693e238
Erik Solen <erik@solen.dev>
2023-12-03 16:13:42 -0500

    day 3 for advent of code

--- apps/nextjs-home/src/app/advent-of-code-2023/page.tsx
+++ apps/nextjs-home/src/app/advent-of-code-2023/page.tsx
@@ -12,7 +12,10 @@ const AdventOfCode2023 = () => {
           Done for <Link href="/advent-of-code-2023/2">Day 2</Link> both parts.
         </li>
         <li>
-          <Link href="/advent-of-code-2023/3">Day 3</Link> under construction.
+          Done for <Link href="/advent-of-code-2023/3">Day 3</Link> both parts.
+        </li>
+        <li>
+          <Link href="/advent-of-code-2023/4">Day 4</Link> under construction.
         </li>
       </ol>
     </main>
