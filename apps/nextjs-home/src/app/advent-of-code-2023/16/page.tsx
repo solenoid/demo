1300d9ee544908781ea6dc67ee6c63644ee98f28
Erik Solen <erik@solen.dev>
2023-12-16 12:46:44 -0500

    progress through day 16 so far

---
+++ apps/nextjs-home/src/app/advent-of-code-2023/16/page.tsx
@@ -0,0 +1,132 @@
+import Link from 'next/link'
+import { readFile } from 'node:fs/promises'
+
+const dirPath = 'src/app/advent-of-code-2023/16'
+const sampleInput = String(await readFile(`${dirPath}/sampleInput.txt`))
+const realInput = String(await readFile(`${dirPath}/realInput.txt`))
+
+// const sumIt = (prev: number, item: number) => prev + item
+
+const useTestInput = false
+const input = useTestInput ? sampleInput : realInput
+
+const wall = input.split('\n').map((line) => line.split(''))
+
+// console.log('wall')
+// console.log(wall.map((d) => d.join('')).join('\n'))
+
+const width = wall[0].length
+const height = wall.length
+type dir = 'N' | 'E' | 'S' | 'W'
+
+// next direction for /
+const forwardSlash = (d: dir): dir =>
+  d === 'N' ? 'E' : d === 'E' ? 'N' : d === 'S' ? 'W' : 'S'
+// next direction for /
+const backSlash = (d: dir): dir =>
+  d === 'N' ? 'W' : d === 'W' ? 'N' : d === 'S' ? 'E' : 'S'
+
+const serialize = (d: dir, x: number, y: number) => `${d}:${x}:${y}`
+
+const findEnergizedCount = (initial: string) => {
+  let beamTips = new Set([initial])
+  let beams = new Set([initial])
+  let energyChange = true
+  let energizedCount = 0
+  while (beamTips.size > 0 && energyChange) {
+    const newTips = Array.from(beamTips)
+      .map((tip) => {
+        const parts = tip.split(':')
+        let d = parts[0] as dir
+        let x = Number(parts[1])
+        let y = Number(parts[2])
+        if (d === 'E') x++
+        if (d === 'N') y--
+        if (d === 'W') x--
+        if (d === 'S') y++
+
+        const next = wall?.[y]?.[x] ?? ''
+        if (!next) {
+          return ''
+        } else {
+          let nextMoves: string[] = []
+          if (next === '/') {
+            d = forwardSlash(d)
+            nextMoves = [serialize(d, x, y)]
+          }
+          if (next === '\\') {
+            d = backSlash(d)
+            nextMoves = [serialize(d, x, y)]
+          }
+          if (next === '-' && (d === 'N' || d === 'S')) {
+            nextMoves = [serialize('E', x, y), serialize('W', x, y)]
+          }
+          if (next === '|' && (d === 'E' || d === 'W')) {
+            nextMoves = [serialize('N', x, y), serialize('S', x, y)]
+          }
+          if (nextMoves.length === 0) {
+            nextMoves = [serialize(d, x, y)]
+          }
+          nextMoves.forEach((nextMove) => {
+            beams.add(nextMove)
+          })
+          return nextMoves
+        }
+      })
+      .flat()
+      .filter(Boolean)
+    newTips.forEach((tip) => {
+      beamTips.add(tip)
+    })
+
+    // allow for loops where beams bounce around forever on the same path
+    if (energizedCount === beams.size) {
+      energyChange = false
+    } else {
+      energizedCount = beams.size
+    }
+  }
+  const energized = Array.from(beams).map((d) => d.slice(2))
+  // console.log('beams')
+  // for (let y = 0; y < height; y++) {
+  //   let line = Array(width).fill('.')
+  //   for (let x = 0; x < width; x++) {
+  //     if (energized.includes(`${x}:${y}`)) {
+  //       line[x] = '#'
+  //     }
+  //   }
+  //   console.log(line.join(''))
+  // }
+  const ret = new Set(energized).size - 1
+  // console.log(ret, initial)
+  return ret
+}
+
+const part1 = findEnergizedCount(serialize('E', -1, 0))
+console.log(part1)
+
+let part2 = 0
+for (let y = 0; y < height; y++) {
+  part2 = Math.max(part2, findEnergizedCount(serialize('E', -1, y)))
+  part2 = Math.max(part2, findEnergizedCount(serialize('W', width, y)))
+}
+for (let x = 0; x < height; x++) {
+  part2 = Math.max(part2, findEnergizedCount(serialize('S', x, -1)))
+  part2 = Math.max(part2, findEnergizedCount(serialize('N', x, height)))
+}
+console.log(part2)
+
+const CoDeO = () => {
+  return (
+    <main>
+      <h2>Day 26</h2>
+      <p>Part 1: calculated is {part1}, and answer is 7199.</p>
+      <p>Part 2: calculated is {part2}, and answer is 7438.</p>
+      <p>
+        <Link href="/advent-of-code-2023">Back to Advent</Link>
+      </p>
+    </main>
+  )
+}
+
+export default CoDeO
--- apps/nextjs-home/src/app/advent-of-code-2023/page.tsx
+++ apps/nextjs-home/src/app/advent-of-code-2023/page.tsx
@@ -6,31 +6,31 @@ const AdventOfCode2023 = () => {
       <p>TBD how much gets done.</p>
       <ol>
         <li>
-          Done for <Link href="/advent-of-code-2023/1">Day 1</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/1">Day 1</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/2">Day 2</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/2">Day 2</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/3">Day 3</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/3">Day 3</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/4">Day 4</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/4">Day 4</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/5">Day 5</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/5">Day 5</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/6">Day 6</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/6">Day 6</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/7">Day 7</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/7">Day 7</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/8">Day 8</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/8">Day 8</Link> both parts
         </li>
         <li>
-          Done for <Link href="/advent-of-code-2023/9">Day 9</Link> both parts.
+          Done for <Link href="/advent-of-code-2023/9">Day 9</Link> both parts
         </li>
         <li>
           Done for <Link href="/advent-of-code-2023/10">Day 10</Link> both parts
@@ -39,10 +39,19 @@ const AdventOfCode2023 = () => {
           Done for <Link href="/advent-of-code-2023/11">Day 11</Link> both parts
         </li>
         <li>
-          <Link href="/advent-of-code-2023/12">Day 12</Link> maybe later
+          Working on <Link href="/advent-of-code-2023/12">Day 12</Link> maybe
         </li>
         <li>
           Done for <Link href="/advent-of-code-2023/13">Day 13</Link> both parts
+        </li>
+        <li>
+          Done for <Link href="/advent-of-code-2023/14">Day 14</Link> part one
+        </li>
+        <li>
+          Done for <Link href="/advent-of-code-2023/15">Day 15</Link> part one
+        </li>
+        <li>
+          Done for <Link href="/advent-of-code-2023/16">Day 16</Link> both parts
         </li>
       </ol>
     </main>
